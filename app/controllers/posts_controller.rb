9b9a4d45a4a26db6ad26fa24b2ae705f3f40d71a
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-10-06 18:09:19 +0100

    feat: add logic to #create action for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [ :create, :new ]
   def create
-    current_user = current_user
-    current_user.posts.create(post_params)
+    new_post = current_user.posts.build(post_params)
+    if new_post.save
+      redirect_to new_post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def new
