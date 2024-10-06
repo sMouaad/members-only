40ea7c522138a9b368a3d5ec74cd1ad1714842b7
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-10-06 17:14:59 +0100

    feat: add devise views and change path from "users" to "auth"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "auth"
   resources :posts, only: [ :new, :create, :index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
