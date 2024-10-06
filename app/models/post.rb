1ea5b171824b480cb9ec50e217415c5cd77557ff
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-10-06 17:05:39 +0100

    feat: add input validations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, :content, presence: true
+  validates :title, length: { minimum: 2 }
+  validates :content, length: { in: 10..200 }
 end
