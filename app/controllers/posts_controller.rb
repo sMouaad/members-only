dd965186249a8fdcc207871daa2fe1e48cfd3e83
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-10-06 17:01:49 +0100

    feat: add view to posts' #new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,19 @@
 class PostsController < ApplicationController
-  before_action :require_login
+  before_action :require_login, only: [ :create, :new ]
+  def create
+  end
 
+  def new
+    @post = Post.new
+  end
 
+  def index
+  end
   private
 
   def require_login
+    unless user_signed_in?
+      flash[:error] = "You should sign in first"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: [:new,:create,:index]
+  resources :posts, only: [ :new, :create, :index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
